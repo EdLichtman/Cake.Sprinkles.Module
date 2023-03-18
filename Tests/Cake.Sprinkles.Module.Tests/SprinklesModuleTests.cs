1abbbb97d0917ca9914fb80e8efa003bafb49afe
Eddie Lichtman <e.lichtman2@gmail.com>
2023-03-17 19:38:56 -0500

    Adds Base for another thing to test (making sure my module can't be overwritten once included)

---
+++ Tests/Cake.Sprinkles.Module.Tests/SprinklesModuleTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake.Sprinkles.Module.Tests
+{
+    [TestFixture]
+    internal class SprinklesModuleTests
+    {
+        [Test]
+        public void IncludingSprinklesModuleDoesNotPreventUserFromAddingTheirOwnTaskSetup()
+        {
+            throw new NotImplementedException("Will probably fail and that's why I think I need to reach out to the Cake Team.");
+        }
+    }
+}
